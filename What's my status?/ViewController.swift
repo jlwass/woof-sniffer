6389f2c36976c778f8812bc1d5483844cf30685b
Jessica Wass <jlwass@mit.edu>
2015-09-19 14:15:30 -0400

    Initial Commit

---
+++ What's my status?/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  What's my status?
+//
+//  Created by Jessica Wass on 9/19/15.
+//  Copyright (c) 2015 Jessica Wass. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
